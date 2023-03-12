3e6ce1a5fa22e97088023bc454cf6adccdb0118b
Samuel Gichuhi <gichuhi1sam@gmail.com>
2023-03-12 20:24:14 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Write your solution in this file!
+
+const employee = {
+    name: "Sam",
+    streetAddress:"Eluid",
+};
+
+
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+const tempOb ={...employee};
+tempOb[key]=value;
+return tempOb;
+}
+
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value){
+employee[key]=value;
+return employee;
+}
+
+
+function deleteFromEmployeeByKey(employee,key){
+
+let temp ={...employee};
+delete temp[key];
+return temp
+
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee,key){ 
+    delete employee[key];
+    return employee;
+     
+    }
